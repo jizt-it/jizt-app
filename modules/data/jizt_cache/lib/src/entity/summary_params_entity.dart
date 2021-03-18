b0457f14aedfe26c06fb286594cb25ffb282c02f
David Miguel Lozano <me@davidmiguel.com>
2021-03-18 22:55:29 +0100

    #3: Migrate jizt_cache package to null safety

--- modules/data/jizt_cache/lib/src/entity/summary_params_entity.dart
+++ modules/data/jizt_cache/lib/src/entity/summary_params_entity.dart
@@ -5,27 +5,27 @@ part 'summary_params_entity.g.dart';
 @HiveType(typeId: 1)
 class SummaryParamsEntity extends HiveObject {
   @HiveField(0)
-  final double relativeMaxLength;
+  final double? relativeMaxLength;
   @HiveField(1)
-  final double relativeMinLength;
+  final double? relativeMinLength;
   @HiveField(2)
-  final bool doSample;
+  final bool? doSample;
   @HiveField(3)
-  final bool earlyStopping;
+  final bool? earlyStopping;
   @HiveField(4)
-  final int numBeams;
+  final int? numBeams;
   @HiveField(5)
-  final int temperature;
+  final int? temperature;
   @HiveField(6)
-  final int topK;
+  final int? topK;
   @HiveField(7)
-  final int topP;
+  final int? topP;
   @HiveField(8)
-  final int repetitionPenalty;
+  final int? repetitionPenalty;
   @HiveField(9)
-  final int lengthPenalty;
+  final int? lengthPenalty;
   @HiveField(10)
-  final int noRepeatNgramSize;
+  final int? noRepeatNgramSize;
 
   SummaryParamsEntity({
     this.relativeMaxLength,
