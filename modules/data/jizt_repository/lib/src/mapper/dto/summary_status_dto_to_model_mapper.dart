1b40002bb588ca017fb9cf6c44a15370f3a2d10b
David Miguel Lozano <me@davidmiguel.com>
2021-12-29 17:58:29 +0100

    Upgrade to Flutter 2.8.0 and Dart 2.15.0

--- modules/data/jizt_repository/lib/src/mapper/dto/summary_status_dto_to_model_mapper.dart
+++ modules/data/jizt_repository/lib/src/mapper/dto/summary_status_dto_to_model_mapper.dart
@@ -4,8 +4,6 @@ import 'package:jizt_repository/src/mapper/mapper.dart';
 class SummaryStatusDtoToModelMapper extends Mapper<String, SummaryStatus> {
   @override
   SummaryStatus map(String input) {
-    return SummaryStatus.values.firstWhere(
-        (status) => status.toString().split('.').last == input,
-        orElse: () => SummaryStatus.unknown);
+    return SummaryStatus.values.byName(input);
   }
 }
